0416e6018d11197f188d91f8c5e330a7b72bd221
amosneculau <amosmadalin.neculau@uzh.ch>
2021-05-08 20:20:59 +0200

    SNIC - Simple Non-Iterative Clustering algorithm for generationg superpixels

---
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/segmentation/Main.java
@@ -0,0 +1,11 @@
+package org.vitrivr.cineast.core.segmentation;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Snic snic = new Snic(-1, -1, "C:\\Dev\\fork\\cineast\\cineast-core\\src\\main\\java\\org\\vitrivr\\cineast\\core\\segmentation\\bee.jpg", -1);
+    }
+}
---
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/segmentation/Node.java
@@ -0,0 +1,7 @@
+package org.vitrivr.cineast.core.segmentation;
+
+public class Node {
+    public int i; // the x and y values packed into one
+    public int k; // the label
+    public double d; // the distance
+}
